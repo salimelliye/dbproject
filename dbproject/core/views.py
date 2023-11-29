3e9ade553bfcec8dd1ff37cae6aae63a16b4c150
Nour <nourmikhael@hotmail.com>
2023-11-29 11:42:56 +0200

    login and signup

--- dbproject/core/views.py
+++ dbproject/core/views.py
@@ -8,3 +8,15 @@ def home(request, *args, **kwargs):
 
     }
     return render(request, 'home.html', context)
+
+def sign_up(request, *args, **kwargs):
+    context = {
+
+    }
+    return render(request, 'signup.html', context)
+
+def login(request, *args, **kwargs):
+    context = {
+
+    }
+    return render(request, 'login.html', context)
--- dbproject/dbproject/urls.py
+++ dbproject/dbproject/urls.py
@@ -22,5 +22,7 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home , name='home'),
+    path('signup/', views.sign_up, name='signup'),
+    path('login/', views.login, name='login')
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
